ef6c5f68df27ed0b4c15c4c009099e48247513ae
monaanvari <38119560+monaanvari@users.noreply.github.com>
2022-08-08 13:35:03 -0700

    Update networks.py

--- deepconsensus/models/networks.py
+++ deepconsensus/models/networks.py
@@ -278,8 +278,8 @@ class EncoderOnlyTransformer(tf.keras.Model):
         with tf.name_scope('add_pos_encoding'):
           pos_encoding = self.position_embedding(inputs=encoder_inputs)
           pos_encoding = tf.cast(pos_encoding, tf.experimental.numpy.float16)
-          print(pos_encoding.type)
-          print(encoder_inputs.type)
+          print(pos_encoding.dtype)
+          print(encoder_inputs.dtype)
           print("HERE!")
           encoder_inputs += pos_encoding
 
